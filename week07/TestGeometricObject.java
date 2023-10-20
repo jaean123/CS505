e5721af7f893af87f621b47f5b2b669e8b722b14
Jan <jaean37@gmail.com>
2023-10-20 19:58:45 -0400

    Create TestGeometricObject.java

---
+++ week07/TestGeometricObject.java
@@ -0,0 +1,20 @@
+public class TestGeometricObject {
+    public static void main(String[] args) {
+        Object obj = new Circle(); // Implicit casting.
+        Object o = new GeometricObject();
+        System.out.println(o.toString());
+        Circle circle = new Circle(1);
+        System.out.println("A circle " + circle.toString());
+        System.out.println("The color is " + circle.getColor());
+        System.out.println("The radius is " + circle.getRadius());
+        System.out.println("The area is " + circle.getArea());
+        System.out.println("The diameter is " + circle.getDiameter());
+
+        Rectangle rectangle =
+                new Rectangle(2, 4);
+        System.out.println("\nA rectangle " + rectangle.toString());
+        System.out.println("The area is " + rectangle.getArea());
+        System.out.println("The perimeter is " +
+                rectangle.getPerimeter());
+    }
+}
