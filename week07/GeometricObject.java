c902acb3bea9415f67937889e1018588b37d1971
Jan <jaean37@gmail.com>
2023-10-20 19:55:50 -0400

    initial commit

---
+++ week07/GeometricObject.java
@@ -0,0 +1,88 @@
+
+public class GeometricObject {
+    public static void main(String[] args) {
+        Object obj = new Circle(); // Implicit casting.
+        Object o = new GeometricObject();
+        System.out.println(o.toString());
+        Circle circle = new Circle(1);
+        System.out.println("A circle " + circle.toString());
+        System.out.println("The color is " + circle.getColor());
+        System.out.println("The radius is " + circle.getRadius());
+        System.out.println("The area is " + circle.getArea());
+        System.out.println("The diameter is " + circle.getDiameter());
+
+        Rectangle rectangle =
+                new Rectangle(2, 4);
+        System.out.println("\nA rectangle " + rectangle.toString());
+        System.out.println("The area is " + rectangle.getArea());
+        System.out.println("The perimeter is " +
+                rectangle.getPerimeter());
+    }
+
+    private String color = "white";
+
+    private boolean filled;
+
+    private java.util.Date dateCreated;
+
+    /**
+     * Construct a default geometric object
+     */
+    public GeometricObject() {
+        dateCreated = new java.util.Date();
+    }
+
+    /**
+     * Construct a geometric object with the specified color
+     * and filled value
+     */
+    public GeometricObject(String color, boolean filled) {
+        dateCreated = new java.util.Date();
+        this.color = color;
+        this.filled = filled;
+    }
+
+    /**
+     * Return color
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * Set a new color
+     */
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    /**
+     * Return filled. Since filled is boolean,
+     * its get method is named isFilled
+     */
+    public boolean isFilled() {
+        return filled;
+    }
+
+    /**
+     * Set a new filled
+     */
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    /**
+     * Get dateCreated
+     */
+    public java.util.Date getDateCreated() {
+        return dateCreated;
+    }
+
+    /**
+     * Return a string representation of this object
+     */
+    public String toString() {
+        return "created on " + dateCreated + "\ncolor: " + color +
+                " and filled: " + filled;
+    }
+}
