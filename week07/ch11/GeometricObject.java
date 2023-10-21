e3ae8d09e6f1a3454b86e556e64b6b3752d8af06
Jan <jaean37@gmail.com>
2023-10-21 09:00:22 -0400

    changes

--- week07/ch11/GeometricObject.java
+++ week07/ch11/GeometricObject.java
@@ -1,24 +1,5 @@
 
 public class GeometricObject {
-    public static void main(String[] args) {
-        Object obj = new Circle(); // Implicit casting.
-        Object o = new GeometricObject();
-        System.out.println(o.toString());
-        Circle circle = new Circle(1);
-        System.out.println("A circle " + circle.toString());
-        System.out.println("The color is " + circle.getColor());
-        System.out.println("The radius is " + circle.getRadius());
-        System.out.println("The area is " + circle.getArea());
-        System.out.println("The diameter is " + circle.getDiameter());
-
-        Rectangle rectangle =
-                new Rectangle(2, 4);
-        System.out.println("\nA rectangle " + rectangle.toString());
-        System.out.println("The area is " + rectangle.getArea());
-        System.out.println("The perimeter is " +
-                rectangle.getPerimeter());
-    }
-
     private String color = "white";
 
     private boolean filled;
@@ -82,7 +63,7 @@ public class GeometricObject {
      * Return a string representation of this object
      */
     public String toString() {
-        return "created on " + dateCreated + "\ncolor: " + color +
-                " and filled: " + filled;
+        return "createtion date: " + dateCreated + ", color: " + color +
+                ", filled: " + filled;
     }
 }
