3be8ee7e23ab308cbfd37450d85ba42dfc5e2c4d
Jan <jaean37@gmail.com>
2023-10-21 09:01:22 -0400

    Create GeometricObject.java

---
+++ week07/ch13/GeometricObject.java
@@ -0,0 +1,46 @@
+import java.util.Date;
+
+public abstract class GeometricObject {
+    private String color = "white";
+    private boolean filled;
+    private java.util.Date dateCreated;
+
+    protected GeometricObject() {
+        dateCreated = new java.util.Date();
+    }
+
+    protected GeometricObject(String color, boolean filled) {
+        dateCreated = new java.util.Date();
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public abstract double getArea();
+
+    public abstract double getPerimeter();
+
+    @Override
+    public String toString() {
+        return "creation date: " + dateCreated + ", color: " + color + ", filled: " + filled;
+    }
+}
