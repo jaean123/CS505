d3ca5e23d5734cb08f64931d2eef44ecbdb55017
Jan <jaean37@gmail.com>
2023-11-06 18:03:39 -0500

    Updated code for clarity and more comments.

--- week09/CustomQueue.java
+++ week09/CustomQueue.java
@@ -2,27 +2,6 @@ import java.util.Iterator;
 import java.util.LinkedList;
 
 public class CustomQueue<T> implements Iterable<T> {
-    public static void main(String[] args) {
-        CustomQueue<String> lineOfCustomers = new CustomQueue<>();
-        lineOfCustomers.enqueue("Bob");
-        lineOfCustomers.enqueue("Sally");
-        lineOfCustomers.enqueue("Dan");
-        lineOfCustomers.enqueue("Billy");
-        lineOfCustomers.enqueue("Kate");
-
-        System.out.print("There are " + lineOfCustomers.size() + " customers on line are as follows: ");
-        Iterator<String> iterator = lineOfCustomers.iterator();
-        while (iterator.hasNext()) {  // Alternativelly, you can use the for-each statement.
-            System.out.print(iterator.next() + " ");
-        }
-        System.out.println();
-
-        System.out.println(lineOfCustomers.peek() + " is the first one on line.");
-        while (!lineOfCustomers.isEmpty()) {
-            System.out.println(lineOfCustomers.dequeue() + " has been served.");
-        }
-    }
-
     private LinkedList<T> elements = new LinkedList<T>();
 
     /** Adds element to queue */
--- week09/TestArrayAndLinkedList.java
+++ week09/TestArrayAndLinkedList.java
@@ -5,12 +5,16 @@ import java.util.ListIterator;
 
 public class TestArrayAndLinkedList {
     public static void main(String[] args) {
-        List<Integer> arrayList = new ArrayList<>();
+        ArrayList<Integer> arrayList = new ArrayList<>();
         arrayList.add(1);
         arrayList.add(2);
         arrayList.add(0);
         arrayList.add(0, 10);  // Insert 10 at index 0
         arrayList.add(3, 30);  // Insert 30 at index 3
+        ArrayList<Integer> arrayList2 = new ArrayList<>();
+        arrayList2.add(5);
+        arrayList2.add(7);
+        System.out.println(arrayList.addAll(arrayList2));
 
         System.out.println("A list of integers in the array list:");
         System.out.println(arrayList);
@@ -19,21 +23,5 @@ public class TestArrayAndLinkedList {
         linkedList.add(1, "red");
         linkedList.removeLast();
         linkedList.addFirst("green");
-
-        System.out.println("Display the linked list forward:");
-        // Iterator is a universal interface to traverse any Collection.
-        // ListIterator is specific to lists and provides bidirectional iteration.
-        ListIterator<Object> listIterator = linkedList.listIterator();
-        while (listIterator.hasNext()) {
-            System.out.print(listIterator.next() + " ");
-        }
-
-        System.out.println("Display linked list backwards:");
-        // Notice that the parameter to listIterator is the position not the index.
-        // This is why linkedList.size() works. Position starts counting from 1.
-        listIterator = linkedList.listIterator(linkedList.size());
-        while (listIterator.hasPrevious()) {
-            System.out.print(listIterator.previous() + " ");
-        }
     }
 }
