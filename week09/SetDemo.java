3794d38c5c1afff37edf636f93dd37400d261ab3
jaean123 <jaean37@gmail.com>
2024-04-01 14:18:25 -0400

    Update SetDemo.java

--- week09/SetDemo.java
+++ week09/SetDemo.java
@@ -1,7 +1,7 @@
 import java.util.HashSet;
 import java.util.Set;
 
-public class SetExample {
+public class SetDemo {
 
     public static void main(String[] args) {
         Set<String> hashSet = new HashSet<>();
