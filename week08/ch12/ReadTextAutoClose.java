b5d63ecc776d7f19701c84ef8817fc1666676a19
jaean123 <jaean37@gmail.com>
2024-04-01 11:25:42 -0400

    Update ReadTextAutoClose.java

--- week08/ch12/ReadTextAutoClose.java
+++ week08/ch12/ReadTextAutoClose.java
@@ -5,6 +5,8 @@ public class ReadTextAutoClose {
     public static void main(String[] args) throws Exception {
         File file = new File("myfile.txt");
 
+        // The object within the () in the try-with-resources block must
+        // implement AutoCloseable interface. 
         try (Scanner input = new Scanner(file)) {
             while (input.hasNext()) {
                 String line = input.nextLine();
