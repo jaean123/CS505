31f0b3ca9ff87615ebfb51cbe67c5b1796959c72
jaean123 <jaean37@gmail.com>
2024-04-01 17:25:57 -0400

    Update and rename TestIterator.java to IteratorDemo.java

--- week09/IteratorDemo.java
+++ week09/IteratorDemo.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class TestIterator {
+public class IteratorDemo {
     public static void main(String[] args) {
         // Create a Collection<String> of states
         List<Integer> collection = new ArrayList<>();
